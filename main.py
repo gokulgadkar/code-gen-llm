696f6a34c5aa5606b192f2161b1fa4a8d46c6394
cosmicdust0 <01fe21bcs177@kletech.ac.in>
2024-09-03 23:03:21 +0530

    commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 # Load environment variables from a .env file
 load_dotenv()
-
+#test comments
 def main():
     st.header("Sensor data sheet summerizer")
     st.title("Pdf analysizer :books:")
